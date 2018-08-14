775e29b748ad8b749a76ef2501ca208bb424b04f
rht0910 <rht0910@outlook.jp>
2018-08-15 03:12:23 +0900

    express

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' })
+  res.redirect('/en')
+})
+
+router.get('/en', (req, res) => {
+  res.render('en/index', {})
+})
+
+router.get('/ja', (req, res) => {
+  res.render('ja/index', {})
 })
 
 module.exports = router
